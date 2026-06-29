eb9c7091ba5e4f3ba31bc023434e54367ce92404
Mustafa Babiker <dafsawat3@gmail.com>
2026-06-29 11:52:33 +0200

    Upload model.py

---
+++ model.py
@@ -0,0 +1,14 @@
+import segmentation_models_pytorch as smp
+
+
+def build_model():
+
+    model = smp.Unet(
+        encoder_name="resnet34",
+        encoder_weights="imagenet",
+        in_channels=4,
+        classes=1,
+        activation=None
+    )
+
+    return model
